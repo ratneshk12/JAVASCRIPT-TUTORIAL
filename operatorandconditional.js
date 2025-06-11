d2c0bc2523a5ba1ae969c877702339078fc946aa
RATNESH <ratneshkumar8699@gmail.com>
2025-06-11 21:24:22 +0530

    Operator & Condtional Statement chap 2

---
+++ operatorandconditional.js
@@ -0,0 +1,194 @@
+/**Chapter 2
+ * operator & conditional statements
+ * 
+ * Arithemetic operators
+ */ 
+
+// let a = 5;
+// let b = 2;
+// console.log("a=", a,"& b =", b);
+
+// console.log("a+b=", a+b);
+// console.log("a-b=", a-b);
+// console.log("a*b=", a*b);
+// console.log("a/b=", a/b);
+// console.log("a%b=", a%b);
+
+// Exponentiation operators
+// console.log("a**b=", a**b);
+
+// unary operators
+// a++ , a= a+1
+// a--, a= a-1
+
+// increment ++
+
+// a = a + 1; //6
+// console.log(a);
+
+// a++;
+// console.log("a=", a);
+
+//Decrrement --
+
+// a = a-1;
+// console.log(a)
+
+// a--;
+// console.log("a=",a);
+
+/**
+ * a++ (post); a--(post)
+ * ++a (pre); --a(pre)
+ */
+
+// console.log("a++", a++); //post incre
+
+// console.log ("++a", ++a); // pre incre
+
+// console.log("a--", a--); // post
+
+// console.log("--a", --a); // pre
+
+
+// Assignment Operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a=", a,"& b =", b);
+
+// a += 4; // a= a+4
+//console.log("a=", a); // 9
+
+// a -= 4; // a= a-4
+// console.log("a=", a); // 1
+
+// a *= 4; // a= a*4
+// console.log("a=", a); // 20
+
+// a /= 4; // a= a/4
+// console.log("a=", a); // 1.25
+
+// a %= 4; // a= a%4
+// console.log("a=", a); // 1
+
+// a **= 4; // a= a**4
+// console.log("a=", a); // 625
+
+
+//Comparison Operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a == b", a == b);
+
+
+// let a = 5;
+// let b = 5;
+
+// console.log("a == b", a == b);
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a != b", a != b);
+
+// let a = 5;
+// let b = 5;
+
+// console.log("a != b", a != b);
+
+
+// let a = 5; // number
+// let b = "5"; // string
+
+// console.log("a == b", a == b);// true
+
+// strict === comparison
+
+
+// let a = 5; // number
+// let b ="5"; // string
+
+// console.log("a === b", a === b);// false
+
+
+// logical operators
+
+
+// let a = 5; // number
+// let b = 4; 
+
+// let cond1 = a > b;
+// let cond2 = a === 5;
+
+// console.log("cond1 && cond2 =", cond1 && cond2);// true
+
+// let a = 6; // number
+// let b = 4; 
+
+// let cond1 = a > b;
+// let cond2 = a === 5;
+
+// console.log("cond1 && cond2 =", cond1 && cond2);// false
+
+// LOGICAL TABLE
+
+// cond1      connd2    result(&&)   (||)
+//   T           T         T           T
+/**  T           F         F           T
+ *   F           T         F           T
+ *   F           F         F           F
+ */
+
+
+// CONDITIONAL STATEMENTS
+
+/**
+ * let color;
+ * if(mode === "dark-mode") {
+ * color= "black";
+ * }
+ */
+
+// let age = 15;
+
+// if(age >= 18) {
+//     console.log("you can vote");
+// }
+
+// if (age < 18 ) {
+//     console.log("you cannot vote");
+// }
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+
+// if (mode === "light"){
+//     color = "white";
+// }
+
+// console.log(color);
+
+// let age = 16;
+
+// if (age >= 18) {
+//     console.log("vote");
+// } else {
+//     console.log("note vote");
+// }
+
+//odd even
+
+
+
+
+
+
+
